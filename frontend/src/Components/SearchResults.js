362deacb0f465b64f44c2d6712af3ffdf36d319a
haydercharfeddin <haydercharfeddin51@gmail.com>
2023-08-29 18:04:41 +0100

    search

--- frontend/src/Components/SearchResults.js
+++ frontend/src/Components/SearchResults.js
@@ -1,26 +1,25 @@
-import React from "react";
+import React from 'react';
 import "../css/SearchResults.css";
 
-export const SearchResults = () => {
+export const SearchResults = ({ searchResults }) => {
   return (
     <div className="user-view">
-      <div className="div">
-        <div className="overlap">
-          <div className="text-wrapper">Steak House</div>
-          <div className="group">
-            <img className="star" alt="Star" src="star-1.svg" />
-            <img className="img" alt="Star" src="star-2.svg" />
-            <img className="star-2" alt="Star" src="star-3.svg" />
-            <img className="star-3" alt="Star" src="star-4.svg" />
-            <img className="star-4" alt="Star" src="star-5.svg" />
+      {searchResults.map((result, index) => (
+        <div className="div" key={index}>
+          <div className="overlap">
+            <div className="text-wrapper">{result.name}</div>
+            <div className="group">
+              {[1, 2, 3, 4, 5].map((starIndex) => (
+                <img key={starIndex} className="star" alt="Star" src={`star-${starIndex}.svg`} />
+              ))}
+            </div>
+          </div>
+          <div className="overlap-group">
+            <img className="rectangle" alt="Rectangle" src={result.images} />
           </div>
         </div>
-        <div className="overlap-group">
-          <img className="rectangle" alt="Rectangle" src="rectangle-11.png" />
-          <img className="rectangle-2" alt="Rectangle" src="rectangle-12.png" />
-          <img className="rectangle-3" alt="Rectangle" src="image.png" />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
+
