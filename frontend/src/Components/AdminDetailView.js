2ff11dc2f5b7d3f4ee1380ef3ffe35ab802cc06a
Anis-Naouali <anis.naouali@esprit.tn>
2023-08-29 18:34:26 +0100

    half updateAdminDetailView

--- frontend/src/Components/AdminDetailView.js
+++ frontend/src/Components/AdminDetailView.js
@@ -1,7 +1,28 @@
 import React from "react";
 import "../css/AdminDetailView.css";
+// import axios from "axios";
+import {useState} from 'react';
+// import { useNavigate } from 'react-router-dom';
 
 export const AdminDetailView = () => {
+
+
+  const [showForm, setShowForm] = useState(false);
+  const [editButton, setEditButton] = useState(true);
+  const toggle = () => {
+    setShowForm(!showForm);
+    setEditButton(!editButton)
+  };
+
+  const mystyle = {
+    width:"200",
+     height:"100",
+      style:"border:0;",
+       allowfullscreen:"",
+        loading:"lazy",
+         referrerpolicy:"no-referrer-when-downgrade"
+  }
+
   return (
     <div className="business-owner">
       <div className="div">
@@ -25,13 +46,13 @@ export const AdminDetailView = () => {
         <div className="text-wrapper-7">Steak House</div>
         <div className="group-2">
           <div className="text-wrapper-8">70 reviews</div>
-          <img className="star" alt="Star" src="star-1.svg" />
-          <img className="star-2" alt="Star" src="star-2.svg" />
-          <img className="star-3" alt="Star" src="star-3.svg" />
-          <img className="star-4" alt="Star" src="star-4.svg" />
-          <img className="star-5" alt="Star" src="star-5.svg" />
+          <img className="star" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+          <img className="star-2" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+          <img className="star-3" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+          <img className="star-4" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+          <img className="star-5" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
         </div>
-        <img className="rectangle" alt="Rectangle" src="rectangle-16.png" />
+        <img className="rectangle" alt="Rectangle" src="https://images.squarespace-cdn.com/content/v1/568d96e0d8af10935f798e4c/1547577857359-0DGQ011R3OEY6HJZTVYC/steak-house-316-2.jpg?format=1500w" />
         <div className="overlap">
           <p className="there-aren-t-enough">
             There aren’t enough food, service, value or
@@ -44,36 +65,35 @@ export const AdminDetailView = () => {
           <div className="group-3">
             <div className="text-wrapper-8">70 reviews</div>
             <img className="star" alt="Star" src="image.svg" />
-            <img className="star-2" alt="Star" src="star-2-2.svg" />
-            <img className="star-3" alt="Star" src="star-3-2.svg" />
-            <img className="star-4" alt="Star" src="star-4-2.svg" />
-            <img className="star-5" alt="Star" src="star-5-2.svg" />
+            <img className="star-2" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+            <img className="star-3" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+            <img className="star-4" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
+            <img className="star-5" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
           </div>
           <img className="vector" alt="Vector" src="vector-8.svg" />
           <div className="div-wrapper">
             <div className="text-wrapper-10">Write a Response</div>
           </div>
         </div>
+       
         <div className="overlap-2">
-          <p className="lorem-ipsum-dolor">
-            Lorem ipsum dolor sit amet consectetur.
-            <br />
-            Risus sociis a quam sed ultricies enim.
-            <br />
-            Elementum posuere eget condimentum
-            <br />
-            nam sodales nunc a massa elementum.
-            <br />
-            Malesuada placerat tortor pharetra sit morbi
-            <br />
-            turpis massa arcu orci. Est risus vitae
-            <br />
-            condimentum velit.
-          </p>
+      
           <div className="text-wrapper-9">Description</div>
+       
           <div className="overlap-wrapper">
-            <div className="overlap-3">
-              <div className="text-wrapper-11">Edit</div>
+          {showForm ? (
+         <input/>
+     ) : (
+       <p>egzgzon efiozefinozf zefzjoefpzef.</p>
+     )}
+     
+
+            <div className="overlap-3" onClick={toggle} >
+            {editButton ? (
+              <div className="text-wrapper-11" >Edit</div>
+              ) : (
+                <div className="text-wrapper-11" >Submit</div>
+                )}
             </div>
           </div>
         </div>
@@ -84,8 +104,9 @@ export const AdminDetailView = () => {
               <div className="text-wrapper-11">Edit</div>
             </div>
           </div>
-          <img className="mask-group" alt="Mask group" src="mask-group.png" />
+          <iframe  style={mystyle} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12765.591839929415!2d10.1885956!3d36.8808269!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cbe05eaab2f3%3A0x13cff2c443049ee1!2sSalle%20de%20sport%20Gold%20gym%20Ennkhilet%20El%20Ghazela!5e0!3m2!1sfr!2stn!4v1693318657076!5m2!1sfr!2stn" ></iframe>
           <div className="group-4">
+            
             <div className="text-wrapper-12">16:00 - 03:00</div>
             <img className="image" alt="Image" src="image-5.png" />
           </div>
@@ -101,4 +122,5 @@ export const AdminDetailView = () => {
       </div>
     </div>
   );
+
 };
