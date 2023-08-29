59c0684f0073412e979248acaf070ab1a3d3910a
mhadrich <hadrichmalek@gmail.com>
2023-08-29 11:34:37 +0100

    Admin Detailed View ready to go

---
+++ frontend/src/Components/AdminDetailView.js
@@ -0,0 +1,104 @@
+import React from "react";
+import "../css/AdminDetailView.css";
+
+export const AdminDetailView = () => {
+  return (
+    <div className="business-owner">
+      <div className="div">
+        <div className="group">
+          <div className="logo">
+            <img className="img" alt="Group" src="group-2.png" />
+            <div className="text-wrapper">Travelish</div>
+          </div>
+          <div className="navbar">
+            <div className="text-wrapper-2">Restaurants</div>
+            <div className="text-wrapper-3">Hotels</div>
+            <div className="text-wrapper-4">Bars</div>
+            <div className="text-wrapper-5">Activities</div>
+          </div>
+          <div className="sign-in">
+            <div className="overlap-group">
+              <div className="text-wrapper-6">username</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-wrapper-7">Steak House</div>
+        <div className="group-2">
+          <div className="text-wrapper-8">70 reviews</div>
+          <img className="star" alt="Star" src="star-1.svg" />
+          <img className="star-2" alt="Star" src="star-2.svg" />
+          <img className="star-3" alt="Star" src="star-3.svg" />
+          <img className="star-4" alt="Star" src="star-4.svg" />
+          <img className="star-5" alt="Star" src="star-5.svg" />
+        </div>
+        <img className="rectangle" alt="Rectangle" src="rectangle-16.png" />
+        <div className="overlap">
+          <p className="there-aren-t-enough">
+            There aren’t enough food, service, value or
+            <br />
+            atmosphere ratings for Steak House yet.
+            <br />
+            Be one of the first to write a review!
+          </p>
+          <div className="text-wrapper-9">Rating &amp; Reviews</div>
+          <div className="group-3">
+            <div className="text-wrapper-8">70 reviews</div>
+            <img className="star" alt="Star" src="image.svg" />
+            <img className="star-2" alt="Star" src="star-2-2.svg" />
+            <img className="star-3" alt="Star" src="star-3-2.svg" />
+            <img className="star-4" alt="Star" src="star-4-2.svg" />
+            <img className="star-5" alt="Star" src="star-5-2.svg" />
+          </div>
+          <img className="vector" alt="Vector" src="vector-8.svg" />
+          <div className="div-wrapper">
+            <div className="text-wrapper-10">Write a Response</div>
+          </div>
+        </div>
+        <div className="overlap-2">
+          <p className="lorem-ipsum-dolor">
+            Lorem ipsum dolor sit amet consectetur.
+            <br />
+            Risus sociis a quam sed ultricies enim.
+            <br />
+            Elementum posuere eget condimentum
+            <br />
+            nam sodales nunc a massa elementum.
+            <br />
+            Malesuada placerat tortor pharetra sit morbi
+            <br />
+            turpis massa arcu orci. Est risus vitae
+            <br />
+            condimentum velit.
+          </p>
+          <div className="text-wrapper-9">Description</div>
+          <div className="overlap-wrapper">
+            <div className="overlap-3">
+              <div className="text-wrapper-11">Edit</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-4">
+          <div className="text-wrapper-9">Location &amp; contact</div>
+          <div className="overlap-wrapper">
+            <div className="overlap-3">
+              <div className="text-wrapper-11">Edit</div>
+            </div>
+          </div>
+          <img className="mask-group" alt="Mask group" src="mask-group.png" />
+          <div className="group-4">
+            <div className="text-wrapper-12">16:00 - 03:00</div>
+            <img className="image" alt="Image" src="image-5.png" />
+          </div>
+          <div className="group-5">
+            <div className="text-wrapper-13">55555555</div>
+            <img className="mask-group-2" alt="Mask group" src="image.png" />
+          </div>
+          <div className="group-6">
+            <p className="p">14 Rue de Liberté, Tunis</p>
+            <img className="image" alt="Image" src="image-7.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
