e64dcd6fda39ea802cc6fe510f346a2fe4699b04
Anis-Naouali <anis.naouali@esprit.tn>
2023-08-30 13:12:34 +0100

    AdminDetail view ready

--- frontend/src/Components/AdminDetailView.js
+++ frontend/src/Components/AdminDetailView.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../css/AdminDetailView.css";
 // import axios from "axios";
-import {useState} from 'react';
+import { useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
 
 export const AdminDetailView = () => {
+
+  const [contactForm, setContactForm] = useState(false)
+  const [editContact, setEditContact] = useState(true);
+  const toggler = () => {
+    setContactForm(!contactForm);
+    setEditContact(!editContact)
+  };
+
 
 
   const [showForm, setShowForm] = useState(false);
@@ -15,12 +23,12 @@ export const AdminDetailView = () => {
   };
 
   const mystyle = {
-    width:"200",
-     height:"100",
-      style:"border:0;",
-       allowfullscreen:"",
-        loading:"lazy",
-         referrerpolicy:"no-referrer-when-downgrade"
+    width: "200",
+    height: "100",
+    style: "border:0;",
+    allowfullscreen: "",
+    loading: "lazy",
+    referrerpolicy: "no-referrer-when-downgrade"
   }
 
   return (
@@ -71,56 +79,81 @@ export const AdminDetailView = () => {
             <img className="star-5" alt="Star" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Star_full.svg/1005px-Star_full.svg.png" />
           </div>
           <img className="vector" alt="Vector" src="vector-8.svg" />
-          <div className="div-wrapper">
+          {/* <div className="div-wrapper">
             <div className="text-wrapper-10">Write a Response</div>
-          </div>
+          </div> */}
         </div>
-       
+
         <div className="overlap-2">
-      
+
           <div className="text-wrapper-9">Description</div>
-       
           <div className="overlap-wrapper">
-          {showForm ? (
-         <input/>
-     ) : (
-       <p>egzgzon efiozefinozf zefzjoefpzef.</p>
-     )}
-     
+            {showForm ? (
+              <div>  <textarea id="story" name="story" rows="5" cols="33">
+                description here</textarea> </div>
+            ) : (
+              <p> description here.</p>
+            )}
+
 
             <div className="overlap-3" onClick={toggle} >
-            {editButton ? (
-              <div className="text-wrapper-11" >Edit</div>
+              {editButton ? (
+                <div className="text-wrapper-11" >Edit</div>
               ) : (
                 <div className="text-wrapper-11" >Submit</div>
-                )}
+              )}
             </div>
           </div>
         </div>
         <div className="overlap-4">
           <div className="text-wrapper-9">Location &amp; contact</div>
           <div className="overlap-wrapper">
-            <div className="overlap-3">
-              <div className="text-wrapper-11">Edit</div>
+            <div className="overlap-3" onClick={toggler}>
+              {editContact ? (
+                <div className="text-wrapper-11" >Edit</div>
+              ) : (
+                <div className="text-wrapper-11" >Submit</div>
+              )}            </div>
+          </div>
+          <iframe style={mystyle} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12765.591839929415!2d10.1885956!3d36.8808269!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cbe05eaab2f3%3A0x13cff2c443049ee1!2sSalle%20de%20sport%20Gold%20gym%20Ennkhilet%20El%20Ghazela!5e0!3m2!1sfr!2stn!4v1693318657076!5m2!1sfr!2stn" ></iframe>
+
+          {contactForm ? (
+            <div>
+              <div className="group-4">
+                <input />
+              </div>
+
+
+              <div className="group-5">
+                <input />
+              </div>
+              <div className="group-6">
+                <input />
+              </div>
             </div>
-          </div>
-          <iframe  style={mystyle} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12765.591839929415!2d10.1885956!3d36.8808269!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cbe05eaab2f3%3A0x13cff2c443049ee1!2sSalle%20de%20sport%20Gold%20gym%20Ennkhilet%20El%20Ghazela!5e0!3m2!1sfr!2stn!4v1693318657076!5m2!1sfr!2stn" ></iframe>
-          <div className="group-4">
-            
-            <div className="text-wrapper-12">16:00 - 03:00</div>
-            <img className="image" alt="Image" src="image-5.png" />
-          </div>
-          <div className="group-5">
-            <div className="text-wrapper-13">55555555</div>
-            <img className="mask-group-2" alt="Mask group" src="image.png" />
-          </div>
-          <div className="group-6">
-            <p className="p">14 Rue de Liberté, Tunis</p>
-            <img className="image" alt="Image" src="image-7.png" />
-          </div>
+          )
+            : (
+              <div>
+                <div className="group-4">
+                  <div className="text-wrapper-12">16:00 - 03:00</div>
+                  {/* <img className="image" alt="Image" src="image-5.png" /> */}
+                </div>
+
+                <div className="group-5">
+                  <div className="text-wrapper-13">55555555</div>
+                  {/* <img className="mask-group-2" alt="Mask group" src="image.png" /> */}
+                </div>
+
+                <div className="group-6">
+                  <p className="p">14 Rue de Liberté, Tunis</p>
+                  {/* <img className="image" alt="Image" src="image-7.png" /> */}
+                </div>
+              </div>
+            )}
         </div>
       </div>
     </div>
+
   );
 
 };
