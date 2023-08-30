ffebf23f604dbd88ef9728185d606a4aa16b6393
mhadrich <hadrichmalek@gmail.com>
2023-08-30 17:34:26 +0100

    Hayder's work App - Home - NavBar - SearchResults

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/NavBar.css";
 
-export const NavBar = () => {
+const NavBar = () => {
   const navigate=useNavigate()
 
   return (
     <div className="group">
       <div className="logo">
-        <img className="img" alt="Group" src="group-2.png" />
-        <div className="text-wrapper" onClick={()=>{ navigate("/Home" ) }} >Travelish</div>
+        <img className="img" alt="Group" src="https://spng.pngfind.com/pngs/s/669-6691661_new-travel-peeps-travel-agency-logo-png-transparent.png" />
+        <div className="text-wrapper" onClick={()=>{ navigate("/" ) }} >Travelish</div>
       </div>
       <div className="div">
         <div className="text-wrapper-2" onClick={()=>{ navigate("/Restaurants" ) }} >Restaurants</div>
@@ -25,3 +25,5 @@ export const NavBar = () => {
     </div>
   );
 };
+
+export default NavBar;
