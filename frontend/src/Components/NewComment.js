5634e421567e551ef0614f64a018ddebb97df7f3
Anis-Naouali <anis.naouali@esprit.tn>
2023-08-31 17:11:42 +0100

    Add comment half done

--- frontend/src/Components/NewComment.js
+++ frontend/src/Components/NewComment.js
@@ -13,7 +13,7 @@ const NewComment = () => {
           </p>
           <div className="selected-business">
             <div className="overlap-group">
-              <img className="rectangle" alt="Rectangle" src="rectangle-25.svg" />
+              <img className="rectangle" alt="Rectangle" src="https://www.hawksmoornyc.com/wp-content/uploads/Prime-rib-with-sides-2--1024x683.jpg" />
               <div className="text-wrapper">Steak House</div>
               <p className="p">14 Rue de Liberté, Tunis</p>
             </div>
@@ -22,16 +22,20 @@ const NewComment = () => {
           <p className="text-wrapper-2">How would you rate your experience?</p>
           <div className="text-wrapper-3">Write a review</div>
           <div className="group-2">
-            <img className="star" alt="Star" src="star-1.svg" />
-            <img className="img" alt="Star" src="star-2.svg" />
-            <img className="star-2" alt="Star" src="star-3.svg" />
-            <img className="star-3" alt="Star" src="star-4.svg" />
-            <img className="star-4" alt="Star" src="star-5.svg" />
+            <img className="star" alt="Star" src="https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png" />
+            <img className="img" alt="Star" src="https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png" />
+            <img className="star-2" alt="Star" src="https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png" />
+            <img className="star-3" alt="Star" src="https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png" />
+            <img className="star-4" alt="Star" src="https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png" />
           </div>
-          <div className="rectangle-2" />
+          <textarea className="rectangle-2" />
         </div>
+        <div className="buttonPlace">
+      <button className="button">Submit</button>
+      </div>
       </div>
     </div>
+    
   );
 };
 
