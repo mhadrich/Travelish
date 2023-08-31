e7e59dc17b7fa23144ea7fe305b2445052282194
haydercharfeddin <haydercharfeddin51@gmail.com>
2023-08-31 11:36:24 +0100

    nav and sing

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,29 +1,58 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "../css/NavBar.css";
-
-const NavBar = () => {
-  const navigate=useNavigate()
-
+export const NavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation(); // Get the current location
+  // Determine whether to show the full NavBar or just the logo
+  const showFullNavBar = location.pathname !== "/SignIn";
   return (
     <div className="group">
-      <div className="logo">
-        <img className="img" alt="Group" src="https://generation-sessions.s3.amazonaws.com/d930e451834e355cc8fe2872204d2fe0/img/group-2@2x.png" />
-        <div className="text-wrapper" onClick={()=>{ navigate("/" ) }} >Travelish</div>
-      </div>
-      <div className="div">
-        <div className="text-wrapper-2" onClick={()=>{ navigate("/Restaurants" ) }} >Restaurants</div>
-        <div className="text-wrapper-3" onClick={()=>{ navigate("/Hotels" ) }}>Hotels</div>
-        <div className="text-wrapper-4" onClick={()=>{ navigate("/Bars" ) }}>Bars</div>
-        <div className="text-wrapper-5" onClick={()=>{ navigate("/Activities" ) }}>Activities</div>
-      </div>
-      <div className="sign-in">
-        <div className="overlap-group">
-          <div className="text-wrapper-6" onClick={()=>{ navigate("/SignIn" ) }}>Sign in</div>
+      <div className="logo" onClick={() => navigate("/")}>
+        <img
+          className="img"
+          alt="Group"
+          src="https://spng.pngfind.com/pngs/s/669-6691661_new-travel-peeps-travel-agency-logo-png-transparent.png"
+        />
+        <div className="text-wrapper" onClick={() => navigate("/")}>
+          Travelish
         </div>
       </div>
+      {showFullNavBar && (
+        <div className="div">
+          <div
+            className="text-wrapper-2"
+            onClick={() => navigate("/Restaurants")}
+          >
+            Restaurants
+          </div>
+          <div className="text-wrapper-3" onClick={() => navigate("/Hotels")}>
+            Hotels
+          </div>
+          <div className="text-wrapper-4" onClick={() => navigate("/Bars")}>
+            Bars
+          </div>
+          <div
+            className="text-wrapper-5"
+            onClick={() => navigate("/Activities")}
+          >
+            Activities
+          </div>
+        </div>
+      )}
+      {showFullNavBar && (
+        <div className="sign-in">
+          <div className="overlap-group">
+            <div
+              className="text-wrapper-6"
+              onClick={() => navigate("/SignIn")}
+            >
+              Sign in
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-
 export default NavBar;
