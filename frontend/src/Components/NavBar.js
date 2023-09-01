37db4aeee9bf94e18896d6b811969e0de2951803
mhadrich <hadrichmalek@gmail.com>
2023-09-01 18:56:44 +0100

    splash updated

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -5,9 +5,11 @@ export const NavBar = () => {
   const navigate = useNavigate();
   const location = useLocation(); // Get the current location
   // Determine whether to show the full NavBar or just the logo
-  const showFullNavBar = location.pathname !== "/SignIn" && location.pathname !== '/CreateAccount';
+  const showFullNavBar = location.pathname !== "/SignIn" && location.pathname !== '/CreateAccount' && location.pathname !== '/';
   return (
+    
     <div className="group">
+      
       <div className="logo" onClick={() => navigate("/")}>
       <svg className="img" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M40 20C40 31.0457 31.0457 40 20 40C8.9543 40 0 31.0457 0 20C0 8.9543 8.9543 0 20 0C31.0457 0 40 8.9543 40 20Z" fill="#ACF0FF"/>
@@ -62,6 +64,8 @@ export const NavBar = () => {
           </div>
         </div>
       )}
+
+
     </div>
   );
 };
--- frontend/src/Components/SplashScreen.js
+++ frontend/src/Components/SplashScreen.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import '../css/SplashScreen.css'
+import { useNavigate } from "react-router-dom";
 
-function SplashScreen () {
+function SplashScreen() {
+  const navigate = useNavigate();
+  const timer = 2000;
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigate('/Home');
+    }, timer);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div>
-      <img src='https://res.cloudinary.com/dkago8t99/image/upload/v1693586271/Travelish_bgoueb.gif'></img>
+    <div className="splash">
+      <img src="https://res.cloudinary.com/dkago8t99/image/upload/v1693586271/Travelish_bgoueb.gif" alt="Splash" />
     </div>
-  )
+  );
 }
 
-export default SplashScreen
+export default SplashScreen;
